4bf45b8bc61bcc742caece44919c9b61837130bb
krisn2 <krishnasarone08@gmail.com>
2024-12-18 15:10:55 +0530

    Story component done

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,14 +1,49 @@
 import AnimatedTitle from "./AnimatedTitle"
+import gsap from "gsap";
+import { useRef } from "react";
+import RoundedCorners from "./RoundedCorners";
+import Button from "./Button";
 
 const Story = () => {
 
-    // const frameRef = useRef('null');
+    const frameRef = useRef('null');
 
-    // const handleMouseLeave = () => {
-    // }
+    const handleMouseLeave = () => {
+        const element = frameRef.current;
 
-    // const handleMouseMove = (event) => {
-    // }
+        if (element) {
+          gsap.to(element, {
+            duration: 0.3,
+            rotateX: 0,
+            rotateY: 0,
+            ease: "power1.inOut",
+          });
+        }
+    }
+
+    const handleMouseMove = (event) => {
+        const {clientX, clientY} = event;
+        const element = frameRef.current;
+
+        if (!element) return;
+
+        const rect = element.getBoundingClientRect();
+        const x = clientX - rect.left;
+        const y = clientY - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        const rotateX = ((y - centerY) / centerY) * -10;
+        const rotateY = ((x - centerX) / centerX) * 10;
+
+        gsap.to(element, {
+            duration:0.3,
+            rotateX, rotateY,
+            transformPerspective: 500,
+            ease :'power1.inOut',
+        })
+    }
         
 
   return (
@@ -20,31 +55,49 @@ const Story = () => {
                 the multiversal is world
             </p>
 
-            <div className="realtive size-full">
-                <AnimatedTitle
-                title="The story of a hidden realm"
-                sectionId="#story"
-                containerClass="mt-5 pointer-events-none 
-                mix-blend-difference relative z-10 text-center"
+            <div className="relative size-full">
+          <AnimatedTitle
+            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  onMouseMove={handleMouseMove}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  src="/img/entrance.webp"
+                  alt="entrance.webp"
+                  className="object-contain"
                 />
-
-                <div className="story-img-container">
-                    <div className="story-img-mask">
-                        <div className="story-img-content">
-                            <img ref={frameRef}
-                            src="/img/entrance.webp" 
-                            onMouseLeave={handleMouseLeave}
-                            onMouseUp={handleMouseLeave}
-                            onMouseEnter={handleMouseLeave}
-                            onMouseMove={handleMouseMove}
-                            alt="entrance"
-                            className="object-contain"
-                            />
-                        </div>
-                    </div>
-                </div>
-
+              </div>
             </div>
+
+            <RoundedCorners />
+            </div>
+            </div>
+
+            <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+                <div className="flex h-full w-fit flex-col items-center md:items-start">
+                    <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+                    Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+                    </p>
+                    
+                    <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt-5 bg-blue-50"
+            />
+
+                </div>
+            </div>
+
         </div>
     </section>
   )
